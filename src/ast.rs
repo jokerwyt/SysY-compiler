a019acbf09711ea2cbb4de959eb09ffde03817b0
jokerwyt <914554688@qq.com>
2024-02-27 21:22:26 +0800

    lv1: main, return and comments

--- src/ast.rs
+++ src/ast.rs
@@ -1,32 +1,32 @@
 #[derive(Debug)]
 pub struct CompUnit {
-    pub func_def: FuncDef,
+  pub func_def: FuncDef,
 }
   
 #[derive(Debug)]
 pub struct FuncDef {
-    pub func_type: FuncType,
-    pub ident: String,
-    pub block: Block,
+  pub func_type: FuncType,
+  pub ident: String,
+  pub block: Block,
 }
 
 #[derive(Debug)]
 pub enum  FuncType {
-    Int = 0,
+  Int = 0,
 }
 
 #[derive(Debug)]
 pub struct Block {
-    pub stmt: Stmt,
+  pub stmt: Stmt,
 }
 
 #[derive(Debug)]
 pub struct Stmt {
-    pub num: i32
+  pub num: i32
 }
 
 #[derive(Debug)]
 pub struct Ident {
-    pub name: String,
+  pub name: String,
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 pub mod ast;
 
+use ::koopa::ir::builder::ValueBuilder;
 use lalrpop_util::lalrpop_mod;
 use std::env::args;
 use std::fs::read_to_string;
 use std::io::Result;
+
+use ::koopa::ir::*;
+use ::koopa::back::*;
+use:: koopa::ir::builder::BasicBlockBuilder;
+use ::koopa::ir::builder::LocalInstBuilder;
 
 // 引用 lalrpop 生成的解析器
 // 因为我们刚刚创建了 sysy.lalrpop, 所以模块名是 sysy
@@ -14,6 +20,7 @@ fn main() -> Result<()> {
   let mut args = args();
   args.next();
   let mode = args.next().unwrap();
+  assert_eq!(mode, "-koopa");
   let input = args.next().unwrap();
   args.next();
   let output = args.next().unwrap();
@@ -22,9 +29,30 @@ fn main() -> Result<()> {
   let input = read_to_string(input)?;
 
   // 调用 lalrpop 生成的 parser 解析输入文件
-  let ast = sysy::CompUnitParser::new().parse(&input).unwrap();
+  let ast: ast::CompUnit = sysy::CompUnitParser::new().parse(&input).unwrap();
 
   // 输出解析得到的 AST
-  println!("{:?}", ast);
+  println!("{:#?}", ast);
+
+  let mut prog = Program::new();
+  let main = prog.new_func(FunctionData::with_param_names(
+    "@main".into(),
+    vec![],
+    Type::get_i32(),
+  ));
+
+  let main_data = prog.func_mut(main);
+  let bb = main_data.dfg_mut().new_bb().basic_block(Some("%entry".into()));
+  main_data.layout_mut().bbs_mut().extend(vec![bb]);
+
+  let ret_val = main_data.dfg_mut().new_value().integer(ast.func_def.block.stmt.num);
+  let ret = main_data.dfg_mut().new_value().ret(Some(ret_val));
+  main_data.layout_mut().bb_mut(bb).insts_mut().extend([ret]);
+
+  // open the output file
+  let output_file = std::fs::File::create(output)?;
+  let mut generator = KoopaGenerator::new(output_file);
+  generator.generate_on(&prog)?;
+  
   Ok(())
 }
